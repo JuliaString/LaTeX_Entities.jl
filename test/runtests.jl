cf92b26c1a4e211a8e4155b69c43607883ec8d93
ScottPJones <scottjones@alum.mit.edu>
2018-02-21 15:25:05 -0500

    Update build process

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,8 @@ const def = LaTeX_Entities.default
     @test lookupname(def, SubString("My name is Spock", 12)) == ""
     @test lookupname(def, "foobar")   == ""
     @test lookupname(def, "dagger")   == "†" # \u2020
-    @test lookupname(def, "mscrl")    == "𝓁" # \U1f4c1
+    #@test lookupname(def, "mscrl")    == "𝓁" # \U1f4c1
+    @test lookupname(def, "c_l")       == "𝓁" # \U1f4c1
     @test lookupname(def, "nleqslant") == "⩽̸" # \u2a7d\u338
 end
 
@@ -22,7 +23,8 @@ end
     @test isempty(matches(def, "\U1f596"))
     @test isempty(matches(def, SubString("My name is \U1f596", 12)))
     for (chrs, exp) in (("√", ["sqrt", "surd"]),
-                        ("𝓁", ["mscrl"]),
+                        #("𝓁", ["mscrl"]),
+                        ("𝓁", ["c_l"]),
                         ("⩽̸", ["nleqslant"]))
         res = matches(def, chrs)
         @test length(res) >= length(exp)
@@ -34,7 +36,8 @@ end
     @test isempty(longestmatches(def, "\U1f596 abcd"))
     @test isempty(longestmatches(def, SubString("My name is \U1f596", 12)))
     for (chrs, exp) in (("√abcd", ["sqrt", "surd"]),
-                        ("𝓁abcd", ["mscrl"]),
+                        #("𝓁abcd", ["mscrl"]),
+                        ("𝓁abcd", ["c_l"]),
                         ("⩽̸abcd", ["nleqslant"]))
         res = longestmatches(def, chrs)
         @test length(res) >= length(exp)
@@ -46,7 +49,8 @@ end
     @test isempty(completions(def, "ScottPaulJones"))
     @test isempty(completions(def, SubString("My name is Scott", 12)))
     for (chrs, exp) in (("A", ["AA", "AE", "Alpha"]),
-                        ("mtt", ["mtta", "mttthree", "mttzero"]),
+                        #("mtt", ["mtta", "mttthree", "mttzero"]),
+                        ("varp", ["varperspcorrespond", "varphi", "varpi"]),
                         ("nleq", ["nleq", "nleqslant"]))
         res = completions(def, chrs)
         @test length(res) >= length(exp)
